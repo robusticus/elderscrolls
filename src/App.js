90c01d825436c7ab8a3b42e803ba0bd1455fb165
robusticus <65475274+robusticus@users.noreply.github.com>
2020-10-13 11:56:04 -0700

    Commit for Changed Files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SearchInput from './SearchInput';
+import InfiniteScrollCards from './InfiniteScrollCards';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Main component that contains and coordinates child components
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filterObject: "",
+        };
+      this.FilterInput = this.FilterInput.bind(this);
+    }
+
+    FilterInput(filterObject) {
+        this.setState({filterObject: filterObject});
+    }
+    
+    render() {
+        return (
+            <div className="Container">
+              <section className="Header">
+                <div className="StartItem"><h1>Elder Scrolls Legends</h1></div>
+                <div className="EndItem"><SearchInput onChange={this.FilterInput}/></div>
+              </section>
+              <section className="Main">
+                <InfiniteScrollCards filterObject={this.state.filterObject} />
+              </section>
+              <section className="Footer">
+                All content is copyright of its owner, etc. Elder Scrolls and Elder Scrolls Legends are trademarks of Bethesda Softworks.
+              </section>
+            </div>
+        );
+    };
 }
 
 export default App;
